9ad4306e3c3be13882cb7734fc43211b9215232a
KoiFeur <koifeur.github@gmail.com>
2025-09-27 00:11:15 +0200

    initial commit

---
+++ src/main/java/net/video_jeu/starminerreborn/item/ModItems.java
@@ -0,0 +1,19 @@
+package net.video_jeu.starminerreborn.item;
+
+import net.neoforged.bus.api.IEventBus;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.video_jeu.starminerreborn.StarminerReborn;
+
+public class ModItems {
+	public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(StarminerReborn.MOD_ID);
+	
+	public static final DeferredItem<Item> FEUR = ITEMS.register("feur", () -> new Item(new Item.Properties()));
+	
+	public static final DeferredItem<Item> ROSE = ITEMS.register("rose", () -> new Item(new Item.Properties()));
+	
+	public static void register(IEventBus eventBus) {
+		ITEMS.register(eventBus);
+	}
+}
